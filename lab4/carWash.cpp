af229244162b7c323586152464d800d9890bf18d
gabimiric <gabriel.miricinschi@isa.utm.md>
2024-12-01 21:19:59 +0200

    Created the interfaces Dineable and Refuelable. Wrote implementations for PeopleDinner, RobotDinner, ElectricStation and GasStation through the interfaces. The amount of cars served/refueled is constant across all instances of the same class(station).

--- lab4/carWash.cpp
+++ lab4/carWash.cpp
@@ -18,188 +18,250 @@ public:
 	virtual size_t size() const = 0;
 };
 
-template<typename T>
-class ArrayQueue : public Queue<T> {
-private:
-	vector<T> data;
+// template<typename T>
+// class ArrayQueue : public Queue<T> {
+// private:
+// 	vector<T> data;
+//
+// public:
+// 	void enqueue(const T &item) override
+// 	{
+// 		data.push_back(item);
+// 	}
+//
+// 	T dequeue() override
+// 	{
+// 		if (isEmpty()) throw runtime_error("Queue is empty");
+// 		T frontItem = data.front();
+// 		data.erase(data.begin());
+// 		return frontItem;
+// 	}
+//
+// 	T front() const override
+// 	{
+// 		if (isEmpty()) throw runtime_error("Queue is empty");
+// 		return data.front();
+// 	}
+//
+// 	bool isEmpty() const override
+// 	{
+// 		return data.empty();
+// 	}
+//
+// 	size_t size() const override
+// 	{
+// 		return data.size();
+// 	}
+// };
+//
+// template<typename T>
+// class LinkedListQueue : public Queue<T> {
+// private:
+// 	struct Node {
+// 		T value;
+// 		Node *next;
+// 		Node(const T &val) : value(val), next(nullptr) {}
+// 	};
+//
+// 	Node *head = nullptr;
+// 	Node *tail = nullptr;
+// 	size_t count = 0;
+//
+// public:
+// 	~LinkedListQueue()
+// 	{
+// 		while (!isEmpty()) dequeue();
+// 	}
+//
+// 	void enqueue(const T &item) override
+// 	{
+// 		Node *newNode = new Node(item);
+// 		if (tail) tail->next = newNode;
+// 		tail = newNode;
+// 		if (!head) head = newNode;
+// 		++count;
+// 	}
+//
+// 	T dequeue() override
+// 	{
+// 		if (isEmpty()) throw runtime_error("Queue is empty");
+// 		Node *oldHead = head;
+// 		T frontItem = oldHead->value;
+// 		head = head->next;
+// 		if (!head) tail = nullptr;
+// 		delete oldHead;
+// 		--count;
+// 		return frontItem;
+// 	}
+//
+// 	T front() const override
+// 	{
+// 		if (isEmpty()) throw runtime_error("Queue is empty");
+// 		return head->value;
+// 	}
+//
+// 	bool isEmpty() const override
+// 	{
+// 		return count == 0;
+// 	}
+//
+// 	size_t size() const override
+// 	{
+// 		return count;
+// 	}
+// };
+// template<typename T>
+// class CircularQueue : public Queue<T> {
+// private:
+// 	vector<T> data;
+// 	size_t head = 0, tail = 0, count = 0, capacity;
+//
+// public:
+// 	explicit CircularQueue(size_t cap) : data(cap), capacity(cap) {}
+//
+// 	void enqueue(const T &item) override
+// 	{
+// 		if (count == capacity) throw runtime_error("Queue is full");
+// 		data[tail] = item;
+// 		tail = (tail + 1) % capacity;
+// 		++count;
+// 	}
+//
+// 	T dequeue() override
+// 	{
+// 		if (isEmpty()) throw runtime_error("Queue is empty");
+// 		T frontItem = data[head];
+// 		head = (head + 1) % capacity;
+// 		--count;
+// 		return frontItem;
+// 	}
+//
+// 	T front() const override
+// 	{
+// 		if (isEmpty()) throw runtime_error("Queue is empty");
+// 		return data[head];
+// 	}
+//
+// 	bool isEmpty() const override
+// 	{
+// 		return count == 0;
+// 	}
+//
+// 	size_t size() const override
+// 	{
+// 		return count;
+// 	}
+// };
 
-public:
-	void enqueue(const T &item) override
-	{
-		data.push_back(item);
-	}
 
-	T dequeue() override
-	{
-		if (isEmpty()) throw runtime_error("Queue is empty");
-		T frontItem = data.front();
-		data.erase(data.begin());
-		return frontItem;
-	}
+class Dineable {
+protected:
+	virtual void serveDinner(string carID) {};
 
-	T front() const override
-	{
-		if (isEmpty()) throw runtime_error("Queue is empty");
-		return data.front();
-	}
-
-	bool isEmpty() const override
-	{
-		return data.empty();
-	}
-
-	size_t size() const override
-	{
-		return data.size();
-	}
+	virtual ~Dineable() {};
 };
 
-template<typename T>
-class LinkedListQueue : public Queue<T> {
-private:
-	struct Node {
-		T value;
-		Node *next;
-		Node(const T &val) : value(val), next(nullptr) {}
-	};
+class Refuelable {
+protected:
+	virtual void refuel(string carID) {};
 
-	Node *head = nullptr;
-	Node *tail = nullptr;
-	size_t count = 0;
-
-public:
-	~LinkedListQueue()
-	{
-		while (!isEmpty()) dequeue();
-	}
-
-	void enqueue(const T &item) override
-	{
-		Node *newNode = new Node(item);
-		if (tail) tail->next = newNode;
-		tail = newNode;
-		if (!head) head = newNode;
-		++count;
-	}
-
-	T dequeue() override
-	{
-		if (isEmpty()) throw runtime_error("Queue is empty");
-		Node *oldHead = head;
-		T frontItem = oldHead->value;
-		head = head->next;
-		if (!head) tail = nullptr;
-		delete oldHead;
-		--count;
-		return frontItem;
-	}
-
-	T front() const override
-	{
-		if (isEmpty()) throw runtime_error("Queue is empty");
-		return head->value;
-	}
-
-	bool isEmpty() const override
-	{
-		return count == 0;
-	}
-
-	size_t size() const override
-	{
-		return count;
-	}
+	virtual ~Refuelable() {};
 };
 
-template<typename T>
-class CircularQueue : public Queue<T> {
+class PeopleDinner : public Dineable {
 private:
-	vector<T> data;
-	size_t head = 0, tail = 0, count = 0, capacity;
+	static int servedPeople;
 
 public:
-	explicit CircularQueue(size_t cap) : data(cap), capacity(cap) {}
-
-	void enqueue(const T &item) override
+	void serveDinner(string carID) override
 	{
-		if (count == capacity) throw runtime_error("Queue is full");
-		data[tail] = item;
-		tail = (tail + 1) % capacity;
-		++count;
+		servedPeople++;
+		cout << "Serving dinner to people in " << carID << endl;
 	}
 
-	T dequeue() override
-	{
-		if (isEmpty()) throw runtime_error("Queue is empty");
-		T frontItem = data[head];
-		head = (head + 1) % capacity;
-		--count;
-		return frontItem;
-	}
-
-	T front() const override
-	{
-		if (isEmpty()) throw runtime_error("Queue is empty");
-		return data[head];
-	}
-
-	bool isEmpty() const override
-	{
-		return count == 0;
-	}
-
-	size_t size() const override
-	{
-		return count;
-	}
+	int getServedPeople() const { return servedPeople; }
 };
+
+int PeopleDinner::servedPeople = 0;
+
+class RobotDinner : public Dineable {
+private:
+	static int servedRobot;
+
+public:
+	void serveDinner(string carID) override
+	{
+		servedRobot++;
+		cout << "Serving dinner to robots in " << carID << endl;
+	}
+
+	int getServedRobot() const { return servedRobot; }
+};
+
+int RobotDinner::servedRobot = 0;
+
+class ElectricStation : public Refuelable {
+private:
+	static int fueledElectric;
+
+public:
+	void refuel(string carID) override
+	{
+		fueledElectric++;
+		cout << "Recharging battery for " << carID << endl;
+	}
+
+	int getFueledElectric() const { return fueledElectric; }
+};
+
+int ElectricStation::fueledElectric = 0;
+
+class GasStation : public Refuelable {
+private:
+	static int fueledGas;
+
+public:
+	void refuel(string carID) override
+	{
+		fueledGas++;
+		cout << "Refueling gas for " << carID << endl;
+	}
+
+	int getFueledGas() const { return fueledGas; }
+};
+
+int GasStation::fueledGas = 0;
 
 int main()
 {
-	ArrayQueue<int> intQueue;
-	cout << "ArrayQueue Example:\n";
-	intQueue.enqueue(10);
-	intQueue.enqueue(20);
-	intQueue.enqueue(30);
-	intQueue.enqueue(40);
-	intQueue.enqueue(50);
-	intQueue.enqueue(60);
+	PeopleDinner peopleDinner;
+	RobotDinner robotDinner1, robotDinner2;
 
-	cout << "Size: " << intQueue.size() << "\n";
-	cout << "Front: " << intQueue.front() << "\n";
-	cout << "Dequeued: " << intQueue.dequeue() << "\n";
-	cout << "Front after dequeue: " << intQueue.front() << "\n";
-	cout << "Is Empty: " << intQueue.isEmpty() << "\n";
-	cout << "Size: " << intQueue.size() << "\n\n";
+	ElectricStation electricStation1, electricStation2;
+	GasStation gasStation1, gasStation2;
 
-	LinkedListQueue<string> stringQueue;
-	cout << "LinkedListQueue Example:\n";
-	stringQueue.enqueue("Harro");
-	stringQueue.enqueue("World");
-	stringQueue.enqueue("!!!!!");
-	stringQueue.enqueue("It");
-	stringQueue.enqueue("Me");
-	stringQueue.enqueue("!!!!!");
+	// Dinner
+	peopleDinner.serveDinner("Car1");
+	peopleDinner.serveDinner("Car2");
+	robotDinner1.serveDinner("Car3");
+	robotDinner1.serveDinner("Car5");
+	robotDinner2.serveDinner("Car6");
+	robotDinner2.serveDinner("Car7");
 
-	cout << "Size: " << stringQueue.size() << "\n";
-	cout << "Front: " << stringQueue.front() << "\n";
-	cout << "Dequeued: " << stringQueue.dequeue() << "\n";
-	cout << "Front after dequeue: " << stringQueue.front() << "\n";
-	cout << "Is empty? " << (stringQueue.isEmpty() ? "Yes" : "No") << "\n";
-	cout << "Size: " << stringQueue.size() << "\n\n";
+	// Refuel
+	electricStation1.refuel("Car1");
+	electricStation2.refuel("Car2");
+	electricStation2.refuel("Car3");
+	gasStation1.refuel("Car4");
+	gasStation2.refuel("Car4");
+	gasStation2.refuel("Car6");
 
-	CircularQueue<double> doubleQueue(3);
-	cout << "CircularQueue Example:\n";
-	doubleQueue.enqueue(1.1);
-	doubleQueue.enqueue(2.2);
-	doubleQueue.enqueue(3.3);
-
-	cout << "Size: " << doubleQueue.size() << "\n";
-	cout << "Front: " << doubleQueue.front() << "\n";
-	cout << "Dequeued: " << doubleQueue.dequeue() << "\n";
-	cout << "Is Empty? " << (doubleQueue.isEmpty() ? "Yes" : "No") << "\n";
-	cout << "Front after enqueue: " << doubleQueue.front() << "\n";
-	cout << "Size: " << doubleQueue.size() << "\n";
+	// Show counts
+	cout << "\n---Summary---\n";
+	cout << "People Served Dinner: " << peopleDinner.getServedPeople() << endl;
+	cout << "Robots Served Dinner: " << robotDinner1.getServedRobot() << endl;
+	cout << "Electric cars recharged: " << electricStation1.getFueledElectric() << endl;
+	cout << "Gas cars refueled: " << gasStation1.getFueledGas() << endl;
 
 	return 0;
 }
